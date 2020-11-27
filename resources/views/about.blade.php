0df9526cb9a6af10ecce38b865f6a6a08f92daa7
azamat-alibaev <gatiatullinilnar@gmail.com>
2020-11-27 19:49:42 +0500

    perf(about): blade.php to include header section

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,17 +4,23 @@ Design by TEMPLATED
 http://templated.co
 Released for free under the Creative Commons Attribution License
 
-Name       : Brushwood 
+Name       : Brushwood
 Description: A two-column, fixed-width design with dark color scheme.
 Version    : 1.0
 Released   : 20131025
 
 -->
 <body>
-    @extends ('layout')
+@extends ('layout')
 
-    @section('content')
-    <div id="page-wrapper">О Нас</div>
+@section('content')
+<div id="page-wrapper" class="text-gray-600 text-2xl ">
+   Об индивидуальном предпринимателе.
+</div>
+<div id="page-wrapper" class="text-lg pb-4 p-2 text-center">
+    Здесь можно найти общую информацию по столярному цеху
+    <strong> ИП Алибаев</strong>.
+</div>
 <div id="wrapper">
 	<div id="three-column" class="container"></div>
 	<div id="portfolio" class="container"></div>
@@ -22,4 +28,3 @@ Released   : 20131025
 @endsection
 
 </body>
-</html>
