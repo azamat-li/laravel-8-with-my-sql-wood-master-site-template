275c341bafac7d93f862c0daf7f6b967be886b5e
azamat-li <azamatalifullstack@gmail.com>
2021-01-24 03:35:14 +0500

    fix: no redis in routes

--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,13 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-
 */
-Route::get('/', function () {
-$visits = Redis::incr('visits');
-return view('welcome')->withVisits($visits);
 
- # return view('welcome');
+Route::get('/', function () {
+#$visits = Redis::incr('visits');
+#return view('welcome')->withVisits($visits);
+
+ return view('welcome');
 });
 
 Route::get('/about', function () {
