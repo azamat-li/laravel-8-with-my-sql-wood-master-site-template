e784a79269117398b42a981cb46918c9c6645ad8
azamat-li <azamatalifullstack@gmail.com>
2021-01-24 04:02:06 +0500

    revert: no seo share meta, even removed

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -10,13 +10,6 @@
 <meta
     content="ИП Столярофф Цех"
     name="author"/>
-<!--  
-<meta property="og:title" content="Столярофф Цех" />
-<meta property="og:site_name" content="Столярофф Цех" />
-<meta property="og:description" content="Индивидуальный подход и учёт пожеланий каждого клиента" />
-<meta name="twitter:title" content="Столярофф Цех"/>
-<meta name="twitter:description" content="Индивидуальный подход и учёт пожеланий каждого клиента"/>
--->
 @endsection
 
 @extends ('layout')
@@ -25,7 +18,6 @@
     <div id="page-wrapper">
         <div id="page" class="container">
             <div class="title">
-      <!--          <h2>Рады  <strong>#{{ $visits ?? 'не подсчитываемому пока' }}</strong> просмотру нашей мастерской.</h2> -->
             </div>
             <div id="page-hint"> <strong>Столярофф Цех</strong> - это индивидуальный подход и учёт желаний
                 каждого клиента.
